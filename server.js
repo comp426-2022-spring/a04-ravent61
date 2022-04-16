2613967c4e0f0ea2f9af7cdb8a88a7c98f1aa0d7
Raven Taylor <raven61@live..unc.edu>
2022-04-16 18:09:12 -0400

    copied help message

--- server.js
+++ server.js
@@ -3,22 +3,25 @@
 var argv = require('minimist')(process.argv.slice(2));
 //console.log(argv);
 
-const help_message = "\n" +
-"  --port	Set the port number for the server to listen on. Must be an integer\n" +
-"            between 1 and 65535.\n" +
-"\n" +
-"--debug	If set to `true`, creates endlpoints /app/log/access/ which returns\n" +
-"            a JSON access log from the database and /app/error which throws\n" +
-'            an error with the message "Error test successful." Defaults to\n' +
-"            `false`.\n" +
-"\n" +
-"--log		If set to false, no log files are written. Defaults to true.\n" +
-"           Logs are always written to database.\n" +
-"\n"
-"--help	Return this message and exit.\n"
+const help = (`
+server.js [options]
+
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`)
 
 if (argv['help']) {
-    console.log(help_message);
+    console.log(help);
 }
 
 var express = require("express")
